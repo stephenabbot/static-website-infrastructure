6857de55004a69ebfff43be73656ba507093a69f
Stephen Abbot <abbotnh@yahoo.com>
2025-12-26 13:39:36 -0700

    test

--- modules/domain/main.tf
+++ modules/domain/main.tf
@@ -173,7 +173,13 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   custom_error_response {
-    error_code         = "4xx"
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
     response_code      = 200
     response_page_path = "/index.html"
   }
